a481e13ca4bb37dcc7ce387b995a06287ccf4a44
Alexander Sabourenkov <screwdriver@lxnt.info>
2013-01-27 17:04:43 +0400

    cython binding, discontinued.

--- cyzhban.pyx
+++ cyzhban.pyx
@@ -1,10 +1,13 @@
 cimport cyzhban
-from cpython.buffer cimport PyObject_GetBuffer, PyBuffer_Release, PyBUF_CONTIG_RO
+from cpython.buffer cimport Py_buffer, PyObject_GetBuffer, PyBuffer_Release, PyBUF_CONTIG_RO
+
+class SomeError(Exception):
+    pass
 
 cdef class simprinter:
     cdef cyzhban.zhban_t *_zhban
-    cdef cyzhban.zhban_rect_t *sizer_rv
-    cdef cyzhban.zhban_rect_t *render_rv
+    cdef cyzhban.zhban_rect_t sizer_rv
+    cdef cyzhban.zhban_rect_t render_rv
     
     def __cinit__(self, facebuf, pixheight): # facebuf is bytes, memoryview or something like that..
         cdef Py_buffer buf
@@ -19,7 +22,19 @@ cdef class simprinter:
             cyzhban.zhban_drop(self._zhban)
 
     cdef size(self, text):
-        pass
+        cdef Py_buffer buf
+        PyObject_GetBuffer(text, &buf, PyBUF_CONTIG_RO)
+        fail = zhb_stringrect(self._zhban, <uint16_t *>buf.buf, buf.len, &self.sizer_rv)
+        PyBuffer_Release(&buf)
+        if fail:
+            raise SomeError
 
-    cdef texture(self, text, size):
-        pass
+    cdef texture(self, text):
+        cdef Py_buffer buf
+        PyObject_GetBuffer(text, &buf, PyBUF_CONTIG_RO)
+        fail = zhb_stringtex(self._zhban, <uint16_t *>buf.buf, buf.len, &self.render_rv)
+        PyBuffer_Release(&buf)
+
+        if fail:
+            raise SomeError
+
