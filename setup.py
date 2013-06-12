2091a0455cf1ec30a6e310eb91008f20a5da7900
Alexander Sabourenkov <screwdriver@lxnt.info>
2013-06-12 17:29:21 +0400

    add pkg-config support; link the ext statically

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import subprocess
+
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    output = subprocess.check_output(['pkg-config','--libs', '--cflags' ] + list(packages), universal_newlines=True)
+    for token in output.split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
 
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [
-        Extension("cyzhban", ["cyzhban.pyx"],
-            libraries=["zhban"]
+        Extension("cyzhban", ["cyzhban.pyx", "zhban.c"],
+            extra_compile_args = [ '-std=c99' ],
+            **pkgconfig('harfbuzz', 'freetype2')
         )
     ]
 )
